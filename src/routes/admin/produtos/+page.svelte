7abf40b00f06468e4f78cedd28862b1ecf2a9c7b
André de Oliveira Brandão <brandaoandre00@gmail.com>
2023-10-19 13:33:58 -0300

    update tela admin

--- src/routes/admin/produtos/+page.svelte
+++ src/routes/admin/produtos/+page.svelte
@@ -8,6 +8,7 @@
 	import { Label } from '$lib/components/ui/label';
 	import { cardapioStore } from '$lib/firebase';
 	import FormProduto from '$lib/forms/admin/FormProduto.svelte';
+	import * as Tabs from '$lib/components/ui/tabs';
 
 	$: produtos = $cardapioStore?.produtos ?? [];
 	$: filtered_produtos = produtos.filter((produto) => {
@@ -18,15 +19,18 @@
 		);
 	});
 	let barra_pesquisa = '';
+	$: categorias = [...new Set(filtered_produtos.map((prod) => prod.categoria))];
+
+	function produtosFrom(categoria: string) {
+		return filtered_produtos.filter((prod) => prod.categoria == categoria);
+	}
 </script>
 
 <main class="flex flex-col">
 	<div class="sticky top-0 z-10 p-2 bg-background">
 		<div class="flex flex-row items-center gap-3">
 			<FormProduto action={'create'}>
-				<Button class="ml-1 hover:bg-blue-300">
-					+ Cadastrar Produto
-				</Button>
+				<Button class="ml-1 hover:bg-blue-300">+ Cadastrar Produto</Button>
 			</FormProduto>
 			<div class="grid grid-cols-4 items-center">
 				<Label class="font-bold">Pesquisar:</Label>
@@ -39,13 +43,32 @@
 			</div>
 		</div>
 	</div>
+	<Tabs.Root value={categorias[0]}>
+		<div>
+			<Tabs.List class="flex overflow-y-scroll">
+				{#each categorias as categoria}
+					<Tabs.Trigger class="font-bold data-[state=active]:text-white" value={categoria}
+						>{categoria}</Tabs.Trigger
+					>
+				{/each}
+			</Tabs.List>
+		</div>
+		<!-- {#each filtered_produtos as produto}
+			<FormProduto action={'update'} dfProd={produto}>
+				<CardProduto {produto} />
+			</FormProduto>
+			{/each} -->
 
-	<div class="grid grid-cols-[repeat(auto-fill,minmax(350px,1fr))]">
-		{#each filtered_produtos as produto}
-        <FormProduto action={'update'} dfProd={produto}>
-        
-			<CardProduto {produto} />
-        </FormProduto>
+		{#each categorias as categoria}
+			<Tabs.Content value={categoria}>
+				<div class="grid grid-cols-[repeat(auto-fill,minmax(350px,1fr))]">
+					{#each produtosFrom(categoria) as produto}
+						<FormProduto action={'update'} dfProd={produto}>
+							<CardProduto {produto} />
+						</FormProduto>
+					{/each}
+				</div>
+			</Tabs.Content>
 		{/each}
-	</div>
+	</Tabs.Root>
 </main>
