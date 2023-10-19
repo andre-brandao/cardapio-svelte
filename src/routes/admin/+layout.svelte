26a3748a65ab3bb7dbee334936afa1ff475ea8b9
André de Oliveira Brandão <brandaoandre00@gmail.com>
2023-10-19 13:34:44 -0300

    logo color

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -15,7 +15,7 @@
 		<Doc ref={`admin_permission/${$user.uid}`} let:data>
 			<div class="flex justify-between">
 				<img
-					class="p-2 w-64 h-auto rounded-lg"
+					class="p-2 w-64 h-auto rounded-lg filter invert"
 					src="https://firebasestorage.googleapis.com/v0/b/svelte-cardapio.appspot.com/o/static%2Flogo-h-p.png?alt=media&token=a129a507-20ad-4b9e-887f-d7da1e831155"
 					alt=""
 				/>
